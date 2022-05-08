008fda89090fde52ee3b336914d07365459e7981
Jarvis H Yang <jarvis.h.yang@outlook.com>
2022-05-08 20:08:09 +0800

    Tutorial #2

--- Assets/_Scripts/Manager/InputManager.cs
+++ Assets/_Scripts/Manager/InputManager.cs
@@ -12,11 +12,14 @@ namespace UnityTutorial.Manager
         public Vector2 Move {get; private set;}
         public Vector2 Look {get; private set;}
         public bool Run {get; private set;}
+        public bool Jump {get; private set;}
 
         private InputActionMap _currentMap;
         private InputAction _moveAction;
         private InputAction _lookAction;
         private InputAction _runAction;
+        private InputAction _jumpAction;
+
 
         private void Awake() {
             HideCursor();
@@ -24,16 +27,18 @@ namespace UnityTutorial.Manager
             _moveAction = _currentMap.FindAction("Move");
             _lookAction = _currentMap.FindAction("Look");
             _runAction  = _currentMap.FindAction("Run");
+            _jumpAction = _currentMap.FindAction("Jump");
 
 
             _moveAction.performed += onMove;
             _lookAction.performed += onLook;
             _runAction.performed += onRun;
-
+            _jumpAction.performed += onJump;
 
             _moveAction.canceled += onMove;
             _lookAction.canceled += onLook;
             _runAction.canceled += onRun;
+            _jumpAction.canceled += onJump;
         }
 
         private void HideCursor()
@@ -53,6 +58,11 @@ namespace UnityTutorial.Manager
         private void onRun(InputAction.CallbackContext context)
         {
             Run = context.ReadValueAsButton();
+        }
+        private void onJump(InputAction.CallbackContext context)
+        {
+            Jump = context.ReadValueAsButton();
+
         }
 
         private void OnEnable() {
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -14,12 +14,21 @@ namespace UnityTutorial.PlayerControl
         [SerializeField] private float UpperLimit = -40f;
         [SerializeField] private float BottomLimit = 70f;
         [SerializeField] private float MouseSensitivity = 21.9f;
+        [SerializeField, Range(10, 500)] private float JumpFactor = 260f;
+        [SerializeField] private float Dis2Ground = 0.8f;
+        [SerializeField] private LayerMask GroundCheck;
+        [SerializeField] private float AirResistance = 0.8f;
         private Rigidbody _playerRigidbody;
         private InputManager _inputManager;
         private Animator _animator;
+        private bool _grounded = false;
         private bool _hasAnimator;
         private int _xVelHash;
         private int _yVelHash;
+        private int _jumpHash;
+        private int _groundHash;
+        private int _fallingHash;
+        private int _zVelHash;
         private float _xRotation;
 
         private const float _walkSpeed = 2f;
@@ -36,10 +45,16 @@ namespace UnityTutorial.PlayerControl
 
             _xVelHash = Animator.StringToHash("X_Velocity");
             _yVelHash = Animator.StringToHash("Y_Velocity");
+            _zVelHash = Animator.StringToHash("Z_Velocity");
+            _jumpHash = Animator.StringToHash("Jump");
+            _groundHash = Animator.StringToHash("Grounded");
+            _fallingHash = Animator.StringToHash("Falling");
         }
 
         private void FixedUpdate() {
+            SampleGround();
             Move();
+            HandleJump();
         }
         private void LateUpdate() {
             CamMovements();
@@ -50,8 +65,11 @@ namespace UnityTutorial.PlayerControl
             if(!_hasAnimator) return;
 
             float targetSpeed = _inputManager.Run ? _runSpeed : _walkSpeed;
-            if(_inputManager.Move ==Vector2.zero ) targetSpeed = 0.1f;
+            if(_inputManager.Move ==Vector2.zero) targetSpeed = 0;
 
+            if(_grounded)
+            {
+                
             _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, _inputManager.Move.x * targetSpeed, AnimBlendSpeed * Time.fixedDeltaTime);
             _currentVelocity.y =  Mathf.Lerp(_currentVelocity.y, _inputManager.Move.y * targetSpeed, AnimBlendSpeed * Time.fixedDeltaTime);
 
@@ -59,6 +77,12 @@ namespace UnityTutorial.PlayerControl
             var zVelDifference = _currentVelocity.y - _playerRigidbody.velocity.z;
 
             _playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelDifference, 0 , zVelDifference)), ForceMode.VelocityChange);
+            }
+            else
+            {
+                _playerRigidbody.AddForce(transform.TransformVector(new Vector3(_currentVelocity.x * AirResistance,0,_currentVelocity.y * AirResistance)), ForceMode.VelocityChange);
+            }
+
 
             _animator.SetFloat(_xVelHash , _currentVelocity.x);
             _animator.SetFloat(_yVelHash, _currentVelocity.y);
@@ -72,15 +96,56 @@ namespace UnityTutorial.PlayerControl
             var Mouse_Y = _inputManager.Look.y;
             Camera.position = CameraRoot.position;
 
-            _xRotation -= Mouse_Y * MouseSensitivity * Time.deltaTime;
+            _xRotation -= Mouse_Y * MouseSensitivity * Time.smoothDeltaTime;
             _xRotation = Mathf.Clamp(_xRotation, UpperLimit, BottomLimit);
 
             Camera.localRotation = Quaternion.Euler(_xRotation, 0 , 0);
-            transform.Rotate(Vector3.up, Mouse_X * MouseSensitivity * Time.deltaTime);
+            _playerRigidbody.MoveRotation(_playerRigidbody.rotation * Quaternion.Euler(0, Mouse_X * MouseSensitivity * Time.smoothDeltaTime, 0));
         }
 
+        private void HandleJump()
+        {
+            if(!_hasAnimator) return;
+            if(!_inputManager.Jump) return;
+            _animator.SetTrigger(_jumpHash);
 
+            //Enable this if you want B-Hop
+            //_playerRigidbody.AddForce(-_playerRigidbody.velocity.y * Vector3.up, ForceMode.VelocityChange);
+            //_playerRigidbody.AddForce(Vector3.up * JumpFactor, ForceMode.Impulse);
+            //_animator.ResetTrigger(_jumpHash);
+        }
 
+        public void JumpAddForce()
+        {
+            //Comment this out if you want B-Hop, otherwise the player will jump twice in the air
+            _playerRigidbody.AddForce(-_playerRigidbody.velocity.y * Vector3.up, ForceMode.VelocityChange);
+            _playerRigidbody.AddForce(Vector3.up * JumpFactor, ForceMode.Impulse);
+            _animator.ResetTrigger(_jumpHash);
+        }
 
+        private void SampleGround()
+        {
+            if(!_hasAnimator) return;
+            
+            RaycastHit hitInfo;
+            if(Physics.Raycast(_playerRigidbody.worldCenterOfMass, Vector3.down, out hitInfo, Dis2Ground + 0.1f, GroundCheck))
+            {
+                //Grounded
+                _grounded = true;
+                SetAnimationGrounding();
+                return;
+            }
+            //Falling
+            _grounded = false;
+            _animator.SetFloat(_zVelHash, _playerRigidbody.velocity.y);
+            SetAnimationGrounding();
+            return;
+        }
+
+        private void SetAnimationGrounding()
+        {
+            _animator.SetBool(_fallingHash, !_grounded);
+            _animator.SetBool(_groundHash, _grounded);
+        }
     }
 }
